2dcfa304bb1d842f7437b09b4057ab06e70aa146
TianaGovender <tianagovender1@gmail.com>
2022-07-11 11:00:56 -0700

    had to compile

--- main.h
+++ main.h
@@ -1,10 +1,15 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _putchar(char c);
 int _strlen(char *s);
 int print_char(va_list c);
 int print_string(va_list s);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
 int _printf(const char * const format, ...);
 
 /**
