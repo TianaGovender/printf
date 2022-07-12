557b24eb957ce4a35ef0c07942938fae69545443
TianaGovender <tianagovender1@gmail.com>
2022-07-12 13:34:36 -0700

    test

---
+++ find_format.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ * find_f - finds the relevant funct for the format
+ * @format: char
+ * @args: list
+ * Return: num of char
+ */
+int find_f(const char *format, va_list args)
+{
+	int i = 0, n, f_v, numc = 0;
+	code_format f_fun[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"b", print_bin},
+		{"r", print_rev},
+		{"R", print_rot},
+		{NULL, NULL}
+	};
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			n = 0;
+			while (f_fun[n].cf != NULL)
+			{
+				if (format[i + 1] == f_fun[n].cf[0])
+				{
+					f_v = f_fun[n].f(args);
+					if (f_v == -1)
+						return (-1);
+					numc = numc + f_v;
+					break;
+				}
+				n++;
+			}
+			if (f_fun[n].cf == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					numc = numc + 2;
+				}
+				else
+					return (-1);
+			}
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			numc++;
+		}
+		i++;
+	}
+	return (numc);
+}
